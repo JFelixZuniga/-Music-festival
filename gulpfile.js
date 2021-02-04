b98164fd43f6ecac8d095656922a8e5d38e09f19
JFelixZuniga <johnfzuniga@gmail.com>
2021-02-04 19:33:18 -0300

    5to commit, sección sobre el festival agregada y optimizando imágenes con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@
 // Series nos permite ejecutar las distintas funciones en oden secuencial.
 // Con "parallel" todas las tareas se inician al mismo tiempo
 const { series, src, dest, watch } = require("gulp");
-
 const sass = require("gulp-sass");
+const imagemin = require("gulp-imagemin");
 
 // Función que complila SASS
 function css() {
@@ -20,6 +20,12 @@ function minificarcss() {
     .pipe(dest("./build/css"));
 }
 
+function imagenes() {
+  return src("./src/img/**/*")
+    .pipe(imagemin())
+    .pipe(dest("./build/img"))
+}
+
 // Watch estará constantemente escuchando los cambios en el código, en este caso, los cambios realizados en el archivo "app.scss", y por cada cambio en el código aplicará la función css, el cual compilará de SASS a CSS legible
 function watchArchivos() {
   watch("src/scss/**/*.scss", css);
@@ -27,4 +33,5 @@ function watchArchivos() {
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
