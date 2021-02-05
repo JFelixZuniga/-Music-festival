7cefd832c9a8f4a767eee258ce5f3974e4d9c77f
JFelixZuniga <johnfzuniga@gmail.com>
2021-02-04 22:18:34 -0300

    6to commit, sección de Line Up ok

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,26 @@ const { series, src, dest, watch } = require("gulp");
 const sass = require("gulp-sass");
 const imagemin = require("gulp-imagemin");
 <<<<<<< HEAD
+<<<<<<< HEAD
 const notify = require("gulp-notify");
 const webp = require("gulp-webp");
 const concat = require("gulp-concat");
 
 =======
 >>>>>>> 5to commit, sección sobre el festival agregada y optimizando imágenes con Gulp
+=======
+const notify = require("gulp-notify");
+const webp = require("gulp-webp");
+
+>>>>>>> 6to commit, sección de Line Up ok
 
 const paths = {
   imagenes: "./src/img/**/*",
   scss: "./src/scss/**/*.scss",
+<<<<<<< HEAD
   js: "./src/js/**/*.js"
+=======
+>>>>>>> 6to commit, sección de Line Up ok
 }
 // Función que complila SASS
 function css() {
@@ -53,16 +62,30 @@ function versionWebp(){
     .pipe(notify({message: "Imágenes versión Webp"}));
 =======
 function imagenes() {
-  return src("./src/img/**/*")
+  return src(paths.imagenes)
     .pipe(imagemin())
     .pipe(dest("./build/img"))
+<<<<<<< HEAD
 >>>>>>> 5to commit, sección sobre el festival agregada y optimizando imágenes con Gulp
+=======
+    .pipe(notify({message: "Imágenes minificadas"}));
+}
+
+function versionWebp(){
+  return src(paths.imagenes)
+    .pipe(webp())
+    .pipe(dest("./build/img"))
+    .pipe(notify({message: "Imágenes versión Webp"}));
+>>>>>>> 6to commit, sección de Line Up ok
 }
 
 // Watch estará constantemente escuchando los cambios en el código, en este caso, los cambios realizados en el archivo "app.scss", y por cada cambio en el código aplicará la función css, el cual compilará de SASS a CSS legible
 function watchArchivos() {
   watch(paths.scss, css);
+<<<<<<< HEAD
   watch(paths.js, javascript);
+=======
+>>>>>>> 6to commit, sección de Line Up ok
 }
 
 exports.css = css;
@@ -70,4 +93,8 @@ exports.minificarcss = minificarcss;
 exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
 
+<<<<<<< HEAD
 exports.default = series(css, javascript, imagenes, versionWebp, watchArchivos);
+=======
+exports.default = series(css, imagenes, versionWebp, watchArchivos);
+>>>>>>> 6to commit, sección de Line Up ok
