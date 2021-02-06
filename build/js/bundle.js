579d026d451a408bf193df4a79d57672c87ed12f
JFelixZuniga <johnfzuniga@gmail.com>
2021-02-06 14:37:41 -0300

    corrección de galería e imagen

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   crearGaleria();
 });
 
 function crearGaleria() {
   const galeria = document.querySelector(".galeria-imagenes");
 
-  for(let i = 1; i <= 12; i++) {
+  for (let i = 1; i <= 12; i++) {
     const imagen = document.createElement("img");
     imagen.src = `./build/img/thumb/${i}.webp`;
 
-    imagen.dataset.imagenId = i;  
+    imagen.dataset.imagenId = i;
 
     // Añadir la función de motrarImagen
     imagen.onclick = mostrarImagen;
 
     const lista = document.createElement("li");
     lista.appendChild(imagen);
-    
+
     galeria.appendChild(lista);
   }
 }
@@ -24,15 +24,33 @@ function crearGaleria() {
 function mostrarImagen(e) {
   const id = parseInt(e.target.dataset.imagenId);
 
-  const imagen =  document.createElement("img");
-  imagen.src = imagen.src = `./build/img/grande/${id}.webp`;
-  
+  const imagen = document.createElement("img");
+  imagen.src = `./build/img/grande/${id}.webp`;
+
   const overlay = document.createElement("div");
   overlay.appendChild(imagen);
   overlay.classList.add("overlay");
 
+  overlay.onclick = function () {
+    overlay.remove();
+    body.classList.remove("fijar-body");
+  };
+
+  // Botón para cerrar la imagen
+  const cerrarImagen = document.createElement("p");
+  cerrarImagen.textContent = "X";
+  cerrarImagen.classList.add("btn-cerrar");
+
+  overlay.appendChild(cerrarImagen);
+
+  // Cerrar el overlay al hacer click en la X
+  cerrarImagen.onclick = function () {
+    overlay.remove();
+    body.classList.remove("fijar-body");
+  };
+
   // Mostrar en el Html
   const body = document.querySelector("body");
   body.appendChild(overlay);
-
+  body.classList.add("fijar-body");
 }
